32a7e830eb1d1a3004889efc28fbd4c69638255e
jimmie33 <jimmie33@gmail.com>
2014-04-25 00:01:05 -0400

    added test.bat

---
+++ x64/Release/test.bat
@@ -0,0 +1,17 @@
+REM mkdir F:\projects\Saliency2013\salmaps\SBU_VOC\output_BMS_exp
+REM BMS F:/projects/Saliency2013/data/SBU_VOC/src/ F:/projects/Saliency2013/salmaps/SBU_VOC/output_BMS_exp/ 8 5 5 15 9 1 0
+
+REM mkdir F:\projects\Saliency2013\salmaps\Toronto\output_BMS_exp
+REM BMS F:/projects/Saliency2013/data/Toronto/src/ F:/projects/Saliency2013/salmaps/Toronto/output_BMS_exp/ 8 5 5 15 9 1 0
+
+REM mkdir F:\projects\Saliency2013\salmaps\MIT\output_BMS_exp
+REM BMS F:/projects/Saliency2013/data/MIT/src/ F:/projects/Saliency2013/salmaps/MIT/output_BMS_exp/ 8 5 5 15 9 1 0
+
+REM mkdir F:\projects\Saliency2013\salmaps\Cerf\output_BMS_exp
+REM BMS F:/projects/Saliency2013/data/Cerf/src/ F:/projects/Saliency2013/salmaps/Cerf/output_BMS_exp/ 8 5 5 15 9 1 0
+
+REM mkdir F:\projects\Saliency2013\salmaps\Kootstra\output_BMS_exp
+REM BMS F:/projects/Saliency2013/data/Kootstra/src/ F:/projects/Saliency2013/salmaps/Kootstra/output_BMS_exp/ 8 5 5 15 9 1 0
+
+mkdir F:\projects\Saliency2013\salmaps\ImgSal\output_BMS_exp
+BMS F:/projects/Saliency2013/data/ImgSal/src/ F:/projects/Saliency2013/salmaps/ImgSal/output_BMS_exp/ 8 5 5 15 9 1 0
