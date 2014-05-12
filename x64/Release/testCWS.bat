ef98f146f20a7606ad3f94eafa47b1c8258478a8
jimmie33 <jimmie33@gmail.com>
2014-05-12 17:44:00 -0400

    bat files added

---
+++ x64/Release/testCWS.bat
@@ -0,0 +1,4 @@
+mkdir "Y:\jmzhang_work_dir\projects\Saliency2014\Saliency_Evaluation\Salmaps\MSRA1000\output_CWS\"
+CWS Y:/jmzhang_work_dir/projects/Saliency2014/Saliency_Evaluation/DATA/MSRA1000/ Y:/jmzhang_work_dir/projects/Saliency2014/Saliency_Evaluation/Salmaps/MSRA1000/output_CWS/ 10 0.1 13 0
+
+REM CWS ./src/ ./output/ 10 0.1 13 0
