2f35ff310648e466936f135f88c233264fd39b5a
jimmie33 <jimmie33@gmail.com>
2014-05-16 18:41:36 -0400

    update

--- x64/Release/testBMS.bat
+++ x64/Release/testBMS.bat
@@ -1,17 +1,20 @@
 mkdir F:\projects\Saliency2013\salmapsExp\SBU_VOC\output_BMS_exp
-BMS F:/projects/Saliency2013/data/SBU_VOC/src/ F:/projects/Saliency2013/salmapsExp/SBU_VOC/output_BMS_exp/ 8 5 7 15 9 1 0
+BMS F:/projects/Saliency2013/data/SBU_VOC/src/ F:/projects/Saliency2013/salmapsExp/SBU_VOC/output_BMS_exp/ 8 5 7 9 9 1 0
 
 mkdir F:\projects\Saliency2013\salmapsExp\Toronto\output_BMS_exp
-BMS F:/projects/Saliency2013/data/Toronto/src/ F:/projects/Saliency2013/salmapsExp/Toronto/output_BMS_exp/ 8 5 7 15 9 1 0
+BMS F:/projects/Saliency2013/data/Toronto/src/ F:/projects/Saliency2013/salmapsExp/Toronto/output_BMS_exp/ 8 5 7 9 9 1 0
 
 mkdir F:\projects\Saliency2013\salmapsExp\MIT\output_BMS_exp
-BMS F:/projects/Saliency2013/data/MIT/src/ F:/projects/Saliency2013/salmapsExp/MIT/output_BMS_exp/ 8 5 7 15 9 1 0
+BMS F:/projects/Saliency2013/data/MIT/src/ F:/projects/Saliency2013/salmapsExp/MIT/output_BMS_exp/ 8 5 7 9 9 1 0
 
 mkdir F:\projects\Saliency2013\salmapsExp\Cerf\output_BMS_exp
-BMS F:/projects/Saliency2013/data/Cerf/src/ F:/projects/Saliency2013/salmapsExp/Cerf/output_BMS_exp/ 8 5 7 15 9 1 0
+BMS F:/projects/Saliency2013/data/Cerf/src/ F:/projects/Saliency2013/salmapsExp/Cerf/output_BMS_exp/ 8 5 7 9 9 1 0
 
 mkdir F:\projects\Saliency2013\salmapsExp\Kootstra\output_BMS_exp
-BMS F:/projects/Saliency2013/data/Kootstra/src/ F:/projects/Saliency2013/salmapsExp/Kootstra/output_BMS_exp/ 8 5 7 15 9 1 0
+BMS F:/projects/Saliency2013/data/Kootstra/src/ F:/projects/Saliency2013/salmapsExp/Kootstra/output_BMS_exp/ 8 5 7 9 9 1 0
 
 mkdir F:\projects\Saliency2013\salmapsExp\ImgSal\output_BMS_exp
-BMS F:/projects/Saliency2013/data/ImgSal/src/ F:/projects/Saliency2013/salmapsExp/ImgSal/output_BMS_exp/ 8 5 7 15 9 1 0
+BMS F:/projects/Saliency2013/data/ImgSal/src/ F:/projects/Saliency2013/salmapsExp/ImgSal/output_BMS_exp/ 8 5 7 9 9 1 0
+
+mkdir F:\projects\Saliency2013\salmapsExp\Judd300\output_BMS_exp
+BMS F:/projects/Saliency2013/data/Judd300/src/ F:/projects/Saliency2013/salmapsExp/Judd300/output_BMS_exp/ 8 5 7 9 9 1 0
